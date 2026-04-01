9d5136b69f26976e690b597a24a425bbe9c08664
adityaswaroop1100-jpg <adityaswaroop1100@gmail.com>
2026-04-01 23:56:09 +0530

    Handle command-line argument for user greeting

--- UC2.java
+++ UC2.java
@@ -1,7 +1,10 @@
 public class UC2 {
     public static void main(String[] args) {
-        // Get the name from command-line argument
-        String name = args[0];
-        System.out.println("Hello, " + name + "!");
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
